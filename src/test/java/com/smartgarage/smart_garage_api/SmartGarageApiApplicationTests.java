bb38a8c3a3f1be60048d362b68d531706f1e03a5
Dinshini-Senupama-Thathsarani <dinshinisenupama737@gmail.com>
2026-08-24 11:33:35 +0530

    chore: initialize smart garage api project

---
+++ src/test/java/com/smartgarage/smart_garage_api/SmartGarageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgarage.smart_garage_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGarageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
