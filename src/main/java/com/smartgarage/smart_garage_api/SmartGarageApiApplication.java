27858c62d29663b55d1e8cdb47a8c9f55a1f49ea
Dinshini-Senupama-Thathsarani <dinshinisenupama737@gmail.com>
2026-08-24 20:52:46 +0530

    chore: initialize smart garage api project

---
+++ src/main/java/com/smartgarage/smart_garage_api/SmartGarageApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartgarage.smart_garage_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGarageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGarageApiApplication.class, args);
+	}
+
+}
