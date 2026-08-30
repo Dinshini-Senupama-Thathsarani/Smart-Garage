d8183101f2eeef6cc1267b42efa3fe8b90e897f2
Dinshini-Senupama-Thathsarani <dinshinisenupama737@gmail.com>
2026-08-30 21:03:07 +0530

    customer entity created

---
+++ src/main/java/com/smartgarage/api/entity/Customer.java
@@ -0,0 +1,30 @@
+package com.smartgarage.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true)
+    private User user;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String nic;
+
+    private String phone;
+    private String address;
+}
