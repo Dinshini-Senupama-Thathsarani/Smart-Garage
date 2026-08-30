468668fbba8ebfedf622afa00a4e0fe94bd98b59
Dinshini-Senupama-Thathsarani <dinshinisenupama737@gmail.com>
2026-08-30 21:57:34 +0530

    jwt auth filetr

--- src/main/java/com/smartgarage/api/security/CustomUserDetailsService.java
+++ src/main/java/com/smartgarage/api/security/CustomUserDetailsService.java
@@ -1,4 +1,23 @@
 package com.smartgarage.api.security;
 
-public class CustomUserDetailsService {
+import com.smartgarage.api.entity.User;
+import com.smartgarage.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        return new CustomUserDetails(user);
+    }
 }
