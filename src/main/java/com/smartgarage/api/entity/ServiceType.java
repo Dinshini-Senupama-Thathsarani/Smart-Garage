19d94ee1d048f3cf89beb8330fa6f9fd7ea30f65
Dinshini-Senupama-Thathsarani <dinshinisenupama737@gmail.com>
2026-09-08 15:24:36 +0530

    serviceType entity

---
+++ src/main/java/com/smartgarage/api/entity/ServiceType.java
@@ -0,0 +1,28 @@
+package com.smartgarage.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "service_types")
+public class ServiceType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private ServiceCategory category;
+
+    private String name;
+    private BigDecimal basePrice;
+    private Integer estimatedMinutes;
+}
