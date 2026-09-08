3b96ed89485b5945ee6be9d14e8697e16749dbd3
Dinshini-Senupama-Thathsarani <dinshinisenupama737@gmail.com>
2026-09-08 19:17:32 +0530

    Supplier entity and repo

---
+++ src/main/java/com/smartgarage/api/entity/Supplier.java
@@ -0,0 +1,24 @@
+package com.smartgarage.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "suppliers")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String contactPerson;
+    private String phone;
+    private String email;
+    private String address;
+}
